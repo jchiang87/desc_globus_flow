cacfba7c282856b2afb865c002e6d4472d0c65f3
Jim Chiang <jchiang87@gmail.com>
2026-01-29 09:17:59 -0800

    rebind the flow_function to __main__ namespace to enable serialization

--- bin.src/register_flow_function.py
+++ bin.src/register_flow_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import types
+import __main__
 from desc_globus_flow import get_flow_module, register_flow_function
 
 
@@ -7,5 +9,17 @@ parser = argparse.ArgumentParser()
 parser.add_argument("flow_def_file", type=str, help="flow definition file")
 args = parser.parse_args()
 
-flow_function = get_flow_module(args.flow_def_file).flow_function
+func = get_flow_module(args.flow_def_file).flow_function
+
+# Re-bind the function to the __main__ namespace so that it can be
+# serialized without requiring the flow module to be accessible at the
+# compute site.
+flow_function = types.FunctionType(
+    func.__code__,
+    __main__.__dict__,
+    name=func.__name__,
+    argdefs=func.__defaults__,
+    closure=func.__closure__
+)
+
 register_flow_function(flow_function)
